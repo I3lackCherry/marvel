5b36b83a53ac9f29c6f1e28d11fcba271b56cdf8
I3lackCherry <146437516+I3lackCherry@users.noreply.github.com>
2024-03-28 22:37:26 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:laptev_malkov/main.dart';
+import 'package:comics/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
